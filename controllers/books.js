69b1a9e3a8fd09575cef72755f68075271d2c2a8
Jose De Los Reyes <jddelosreyes23@hotmail.com>
2023-03-30 18:53:31 -0400

    delete function working

--- controllers/books.js
+++ controllers/books.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const books = require("../models/book.js");
 
-// GET: /books/create - show new book form to add a new book
+// GET: /books/create - show new book form to add a new book creating a variable 'booklist' that will contain all the books added to the inventory using the fin() function.
 router.get("/", async (req, res) => {
   try {
     let bookslist = await books.find();
@@ -11,21 +11,36 @@ router.get("/", async (req, res) => {
   } catch (error) {}
 });
 
-// GET: /books/create - show new book form to add a new book
+// GET: /books/create - displays the page of the form
 router.get("/create", (req, res) => {
   res.render("books/create", {});
 });
 
-/* POST: Submit form data to mongodb */
+/* POST: Submit form data to mongodb usign async await */
 router.post("/create", async (req, res) => {
   try {
     await books.create(req.body);
-    console.log("data cretaed!!!!!");
+    console.log("data created!!!");
     res.redirect("/books");
   } catch (error) {
     console.log(error);
-    res.redirect("books/?error=pailas");
+    // we can see that we have an error when it redirect the user.
+    res.redirect("books/?error=there was an error, please check");
   }
+});
+
+// here it will delete the selected employer selecting the id of it. does not need to load a new page just runs the snippet below
+router.get('/delete/:_id', (req, res) => {
+  books.remove({ _id: req.params._id }, (err) => {
+      if (err) {
+          console.log(err);
+      }
+      else {
+          res.redirect('/books');
+          // console message to keep track of errors
+          console.log('There was an issue at the moment to delete this book, please check before you continue on the application')
+      }
+  });
 });
 
 module.exports = router;
